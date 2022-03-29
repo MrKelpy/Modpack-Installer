ca13944d2e1a474722f2b47771cc141bf76ae966
MrKelpy <alexandresilva.coding@gmail.com>
2022-03-29 12:02:40 +0100

    Finished the LogHandler class and implemented its functions on main.py. The program will now pack any latest.log files into a .tar.gz archive.

--- LogHandler.py
+++ LogHandler.py
@@ -7,9 +7,12 @@ If a license applies for this project, the former can be found
 in every distribution, as a "LICENSE" file at top level.
 """
 
-# Built-in Imports
-import os
 import logging
+import os
+import tarfile
+# Built-in Imports
+from typing import List, Union
+
 
 # Third Party Imports
 # Local Application Imports
@@ -25,4 +28,43 @@ class LogHandler:
         self.__logs_path = "./logs"
         self.__log_format = r"[%(levelname)s]: %(message)s"
         self.__latest_log_path = os.path.join("./logs", "latest.log")
-        logging.basicConfig(format=self.__log_format)
+        os.makedirs(self.__logs_path, exist_ok=True)
+        logging.basicConfig(format=self.__log_format, filename=self.__latest_log_path, level=logging.INFO)
+
+
+    def pack_latest(self) -> None:
+        """
+        Archives the latest.log file using .tar.gz compression
+        with the date of logging (As seen in the first line of the logging file)
+        if there is a "latest.log" file.
+        When all is done, clears the file.
+
+        :return:
+        """
+
+        if os.path.exists(self.__latest_log_path):
+
+            if not self._get_latest_log_date():
+                return
+
+            tar = tarfile.open(os.path.join(self.__logs_path, self._get_latest_log_date() + ".tar.gz"), "w:gz")
+            tar.add(self.__latest_log_path, arcname="data.log")
+            tar.close()
+
+            open(self.__latest_log_path, "w").close()  # Clear the latest.log file
+
+
+    def _get_latest_log_date(self) -> Union[None, str]:
+        """
+        Returns the "latest.log" logging date, present at the first line of the logging
+        file.
+        :return: String, the date of logging.
+        """
+
+        with open(self.__latest_log_path, "r") as file:
+            raw_date: List[str] = file.readlines()
+
+        if not raw_date:
+            return
+
+        return raw_date[0].split(":")[1].strip()
--- main.py
+++ main.py
@@ -1,39 +1,23 @@
 """
 This file is distributed as part of the Modpack Installer Project.
 The source code may be available at
-https://github.com/MrKelpy/Modpack Installer 
+https://github.com/MrKelpy/Modpack-Installer
 
 If a license applies for this project, the former can be found
 in every distribution, as a "LICENSE" file at top level.
 """
 
 # Built-in Imports
-import os
 import logging
-import tarfile
 
 # Third Party Imports
 # Local Application Imports
-from globals import LATEST_LOG_PATH
-
-
-def pack_latest() -> None:
-    """
-    Archives the latest.log file using .tar.gz compression
-    with the date of logging (As seen in the first line of the logging file)
-    if there is a latest.log file.
-
-    :return:
-    """
-
-    if os.path.isfile(LATEST_LOG_PATH):
-
-        logging_date: str =
-        with tarfile.open()
+from LaminariaCore.utils.dateutils import get_formatted_date_now
+from LogHandler import LogHandler
 
 if __name__ == "__main__":
-    logging.basicConfig(format="[%(levelname)s]: %(message)s",
-                        filename=)
 
-
-
+    # Handles the logging tasks before the actual program runs
+    loghandler: LogHandler = LogHandler()
+    loghandler.pack_latest()
+    logging.info(get_formatted_date_now(include_seconds=True, formatting=2).replace(":", "."))
